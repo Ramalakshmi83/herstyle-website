9c2d192c42d26b2ec734090789a3e2251dced70b
Ramalakshmi <ramagubbala92@gmail.com>
2026-03-10 13:30:47 +0530

    update style file

--- script.js
+++ script.js
@@ -235,6 +235,20 @@ function changeQty(element, change) {
 function goBack(){
     window.history.back();
 }
+if ("serviceWorker" in navigator) {
+navigator.serviceWorker.register("sw.js")
+.then(() => console.log("Service Worker Registered"));
+}
 
+let deferredPrompt;
+
+window.addEventListener("beforeinstallprompt", (e) => {
+e.preventDefault();
+deferredPrompt = e;
+
+if(confirm("Install HerStyle App?")){
+deferredPrompt.prompt();
+}
+});
 
 
